192a13d7da2513ffc598586c45f4f578e95fd574
Nelson <nelson.atiang@student.moringaschool.com>
2025-03-22 22:45:39 +0300

    complete

--- index.js
+++ index.js
@@ -12,5 +12,43 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial){
+    return tutorial.split(" ").map(function(word){
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    .join(" ")
+  });
 }
+console.log(titleCased())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
